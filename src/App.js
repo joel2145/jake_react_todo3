bbaf3c39cf8ce5258efc85dbd666c8ad06601410
KikuchiYuta <kikkun0513@gmail.com>
2021-07-18 21:33:19 +0900

    JSXでの構造作成とCSSでのスタイリング

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div className="incomplete-area">
+        <p className="title">TODOリスト</p>
+        <ul>
+          <li className="list-row">
+            <p>ああああ</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li className="list-row">
+            <p>いいいい</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了リスト</p>
+        <ul>
+          <li className="list-row">
+            <p>ううううう</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </body>
   );
 }
 
